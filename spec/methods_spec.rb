7f13e89622f130c28a15743bef16c0673b772968
Sergio <sergioobolevich@gmail.com>
2020-10-20 12:10:03 -0300

    Add #make_move methods rspec

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,17 +1,19 @@
 require './lib/logic'
 
 describe Game do
-  let(:new_game) { Game.new }
-  let(:player) { Player.new('Sergio', 'X')}
-  describe '#make_move' do
-      it 'return true if user move is correct' do
-          expect(new_game.make_move(player.team, 3)).to eql TRUE
-      end
-      it 'return false if the move is not a number' do
-          expect(new_game.make_move(player.team, "string")).to eql FALSE
-      end
-      it 'return false if the move is not a number' do
-          expect(new_game.make_move(player.team, 12)).to eql FALSE
-      end
-  end
+    let(:new_game) { Game.new }
+    let(:player) { Player.new('Sergio', 'X')}
+    describe '#make_move' do
+        it 'return true if user move is correct' do
+            expect(new_game.make_move(player.team, 3)).to eql TRUE
+        end
+
+        it 'return false if the move is not a number' do
+            expect(new_game.make_move(player.team, "string")).to eql FALSE
+        end
+
+        it 'return false if the move is not a number' do
+            expect(new_game.make_move(player.team, 12)).to eql FALSE
+        end
+    end
 end
