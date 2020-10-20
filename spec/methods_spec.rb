69077b478ffb35bf50024a9daadc98ddd2c2dfab
Kingsley <kingobaino@gmail.com>
2020-10-20 16:18:19 +0100

    Write test for check_if_win and check_if_move methods

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,19 +1,29 @@
 require './lib/logic'
 
 describe Game do
-    let(:new_game) { Game.new }
-    let(:player) { Player.new('Sergio', 'X')}
-    describe '#make_move' do
-        it 'return true if user move is correct' do
-            expect(new_game.make_move(player.team, 3)).to eql TRUE
-        end
-
-        it 'return false if the move is not a number' do
-            expect(new_game.make_move(player.team, "string")).to eql FALSE
-        end
-
-        it 'return false if the move is not a number' do
-            expect(new_game.make_move(player.team, 12)).to eql FALSE
-        end
+  let(:new_game) { Game.new }
+  let(:player) { Player.new('Sergio', 'X') }
+  describe '#make_move' do
+    it 'return true if user move is correct' do
+      expect(new_game.make_move(player.team, 3)).to eql TRUE
     end
+    it 'return false if the move is not a number' do
+      expect(new_game.make_move(player.team, 'string')).to eql FALSE
+    end
+    it 'return false if the move is not a number between 1-9' do
+      expect(new_game.make_move(player.team, 12)).to eql FALSE
+    end
+  end
+
+  describe '#check_if_win?' do
+    it 'returns false when there is no winner' do
+      expect(new_game.check_if_win?).to eql FALSE
+    end
+  end
+
+  describe '#check_if_move_done?' do
+    it 'returns false if there is no move already' do
+      expect(new_game.check_if_move_done?(7)).to eql FALSE
+    end
+  end
 end
